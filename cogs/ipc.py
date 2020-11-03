76bdea2ae094585fb6f4da44012d2adc43dc5a3d
ziro <palembani@gmail.com>
2020-11-03 15:43:36 +0700

    + You can now able to change prefix from a website via IPC

--- cogs/ipc.py
+++ cogs/ipc.py
@@ -6,10 +6,20 @@ class IpcRoutes(commands.Cog):
         self.bot = bot
     
     @ipc.server.route()
+    async def get_bot_guilds(self, data):
+        return self.bot.guilds 
+    
+    @ipc.server.route()
     async def get_member_count(self, data):
-        guild = self.bot.get_guild(data.guild_id) # get the guild object using parsed guild_id
+        guild = self.bot.get_guild(int(data.guild_id)) # get the guild object using parsed guild_id
 
         return guild.member_count # return the member count to the client
+
+    @ipc.server.route()
+    async def set_prefixes(self, data):
+        guild = self.bot.get_guild(int(data.guild_id))
+        prefixes = list(data.prefixes)
+        self.bot.set_guild_prefixes(guild, prefixes)
 
 def setup(bot):
     bot.add_cog(IpcRoutes(bot))
